3599046938c778bf80a0efa90e23aacd04c0feae
mainanorbert <mainanorbert@gmail.com>
2023-08-23 21:16:56 +0000

    point function that returns bytcode function]

---
+++ compare_func.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * compare_func - compares and returns opcode to execute operation
+ * @str: pointer to first element
+ *
+ * Return: returns a pointer to opcode function
+ */
+void (*compare_func(char *str))(stack_t **, unsigned int)
+{
+	unsigned int i = 0;
+
+	instruction_t instructions[] = {
+		{"push", push_func},
+		{"pall", pall_func},
+		{NULL, NULL}};
+	while (instructions[i].opcode)
+	{
+		if (strcmp(instructions[i].opcode, str) == 0)
+		{
+			return (instructions[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
